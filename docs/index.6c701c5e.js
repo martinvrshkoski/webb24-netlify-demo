3c0bea14a79b6179e13af90631252920dc191ef7
martinvrshkoski <mvrskoski@gmail.com>
2024-10-01 10:28:56 +0200

    commit

---
+++ docs/index.6c701c5e.js
@@ -0,0 +1,2 @@
+console.log("hej"),document.querySelector("h1").style.color="purple";
+//# sourceMappingURL=index.6c701c5e.js.map
---
+++ src/modules/main.js
@@ -0,0 +1,6 @@
+// import _, { map } from 'underscore';
+console.log('hej');
+
+const h1 = document.querySelector('h1');
+h1.style.color = 'purple';
+
